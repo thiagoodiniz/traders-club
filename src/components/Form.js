7e07852dfb89ef8323b693f0c5793da43938e2f0
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-09 17:38:38 -0200

    feat: aplying form state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,10 @@
-import React, {Fragment} from 'react'
+import React, { Component } from 'react'
 import Input from '@material-ui/core/Input'
-//import { InputAdornment } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid';
 
-const styles = theme => ({
+const styles = () => ({
     root: {
       flexGrow: 1,
       padding: 20,
@@ -32,80 +31,109 @@ const styles = theme => ({
     }
   });
 
-const Form = (props) => {
-    const { classes } = props;
-    return(
-            <Fragment>
-                 <form className="form-container">
-                 <div className={classes.root}>
-                    <Grid container spacing={40}>
-                        <Grid item xs={12} >
-                            <Input
-                                className={classes.inputFullWidth}
-                                name="title"
-                                placeholder="Título"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Input
-                                className={classes.inputHalfWidth}
-                                name="model"
-                                placeholder="Modelo" 
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                             <Input
-                                className={classes.inputHalfWidth}
-                                name="year"
-                                placeholder="Ano"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Input
-                                className={classes.inputFullWidth}
-                                name="brand"
-                                placeholder="Montadora"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Input
-                                className={classes.inputHalfWidth}
-                                name="color"
-                                placeholder="Cor" 
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Input
-                                className={classes.inputHalfWidth}
-                                name="price"
-                                placeholder="Preço" 
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Input
-                                className={classes.inputHalfWidth}
-                                name="km"
-                                placeholder="KM"
-                            />
-                        </Grid>
-                    </Grid>
+class Form extends Component{
 
-                    <Grid container spacing={24}>
-                        <Grid item xs={12} sm={2} > 
-                            <Button className={classes.editButton} variant="contained">Remover</Button>
+    state = {
+        title: '',
+        model: '',
+        year: '',
+        brand: '',
+        color: '',
+        price: '',
+        km: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    render(){
+        const { classes } = this.props;
+        return(
+                <form className="form-container">
+                     <div className={classes.root}>
+                        <Grid container spacing={40}>
+                            <Grid item xs={12} >
+                                <Input
+                                    className={classes.inputFullWidth}
+                                    name="title"
+                                    placeholder="Título"
+                                    value={this.state.title}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Input
+                                    className={classes.inputHalfWidth}
+                                    name="model"
+                                    placeholder="Modelo" 
+                                    value={this.state.model}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                 <Input
+                                    className={classes.inputHalfWidth}
+                                    name="year"
+                                    placeholder="Ano"
+                                    value={this.state.year}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Input
+                                    className={classes.inputFullWidth}
+                                    name="brand"
+                                    placeholder="Montadora"
+                                    value={this.state.brand}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Input
+                                    className={classes.inputHalfWidth}
+                                    name="color"
+                                    placeholder="Cor" 
+                                    value={this.state.color}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Input
+                                    className={classes.inputHalfWidth}
+                                    name="price"
+                                    placeholder="Preço" 
+                                    value={this.state.price}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Input
+                                    className={classes.inputHalfWidth}
+                                    name="km"
+                                    placeholder="KM"
+                                    value={this.state.km}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={8} > 
-                            <Button className={classes.editButton} variant="contained">Cancelar</Button>
-                        </Grid>
-                        <Grid item xs={12} sm={1}> 
-                            <Button className={classes.saveButton} variant="contained">Salvar</Button>
-                        </Grid>
-                    </Grid>   
-                    </div>                         
-                </form>
-            
-            </Fragment> 
-    )
+    
+                        <Grid container spacing={24}>
+                            <Grid item xs={12} sm={2} > 
+                                <Button className={classes.editButton} variant="contained">Remover</Button>
+                            </Grid>
+                            <Grid item xs={12} sm={8} > 
+                                <Button className={classes.editButton} variant="contained">Cancelar</Button>
+                            </Grid>
+                            <Grid item xs={12} sm={1} > 
+                                <Button className={classes.saveButton} variant="contained">Salvar</Button>
+                            </Grid>
+                        </Grid>   
+                        </div>                         
+            </form>
+        )     
+    }
+   
 }
 
 export default withStyles(styles)(Form)
