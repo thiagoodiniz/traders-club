a59472dde90b664d64dee087c8e24d33e365f98f
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 18:29:33 -0200

    feat: implemented action and reducer logic for update vehicle

--- src/store/actions/form.js
+++ src/store/actions/form.js
@@ -2,7 +2,9 @@
 export const Types = {
     START_ADD: 'form/START_ADD',
     GET_BRANDS_SUCCESS: 'form/GET_BRANDS_SUCCESS',
-    GET_BRANDS_FAIL: 'form/GET_BRANDS_FAIL'
+    GET_BRANDS_FAIL: 'form/GET_BRANDS_FAIL',
+    START_UPDATE: 'form/START_UPDATE',
+    FINISH_UPDATE: 'form/FINISH_UPDATE' 
 }
 
 export const Creators = {
@@ -16,5 +18,12 @@ export const Creators = {
     getBrandFail: () => ({
         type: Types.GET_BRANDS_FAIL,
         brands: []
+    }),
+    startUpdate: (vehicle) =>({
+        type: Types.START_UPDATE,
+        vehicleToUpdate: vehicle
+    }),
+    finishUpdate: () =>({
+        type: Types.FINISH_UPDATE
     })
 }
--- src/store/reducers/form.js
+++ src/store/reducers/form.js
@@ -2,20 +2,37 @@
 import { Types } from '../actions/form'
 
 const INITIAL_STATE = {
-    brands: []
+    action: null,
+    brands: [],
+    vehicleToUpdate: { }
 }
 
 export default function form(state = INITIAL_STATE, action){
         switch(action.type){
             case Types.START_ADD:
-                return state
-
+                return {
+                    ...state,
+                    action: 'add'
+                }
             case Types.GET_BRANDS_SUCCESS:
             case Types.GET_BRANDS_FAIL:
                 return {
+                    ...state,
                     brands: action.brands
                 }
+            case Types.START_UPDATE:
+                return {
+                    ...state,
+                    action: 'update',
+                    vehicleToUpdate: action.vehicleToUpdate
+                }    
+            
+            case Types.FINISH_UPDATE:
+                return {
+                    INITIAL_STATE
+                }
+
             default:
-                return state    
+                return INITIAL_STATE    
         }
 }
