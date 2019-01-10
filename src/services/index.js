56d01e455df7c24c690bac722755d8dccc6efa9f
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 17:41:32 -0200

    feat: calling search cars api

--- src/services/index.js
+++ src/services/index.js
@@ -2,9 +2,15 @@
 import api from './api'
 
 export const services = {
-    getBrands: () =>{
+    getBrandsRequest: () =>{
        return api.get('brands')
                 .then(resp => resp.data.brands)
                 .catch(err => err)
+    },
+    
+    searchVehiclesRequest: () => {
+        return api.get('cars')
+            .then(resp => resp.data.cars)
+            .catch(err => err)
     }
 }
