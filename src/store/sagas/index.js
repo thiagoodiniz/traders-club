be6a9e876ce7a70baa489f09a2a515c40b40bab8
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 17:42:30 -0200

    feat: config redux-saga for search cars

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,9 +1,11 @@
 import {takeLatest, all } from 'redux-saga/effects'
 import { Types as FormTypes } from '../actions/form'
-import { getBrands } from './list'
+import { Types as ListTypes} from '../actions/list'
+import { getBrands, getCars } from './list'
 
 export default function* rootSaga() {
         return yield all([
             takeLatest(FormTypes.START_ADD, getBrands),
+            takeLatest(ListTypes.START_SEARCH_VEHICLES, getCars)
         ])
 }
--- src/store/sagas/list.js
+++ src/store/sagas/list.js
@@ -1,13 +1,24 @@
 import { call, put } from 'redux-saga/effects'
 import { services } from '../../services'
 import { Creators as FormActions } from '../actions/form'
+import { Creators as ListActions } from '../actions/list'
 
 export function* getBrands(){
     try{
-        const brands = yield call(services.getBrands)
+        const brands = yield call(services.getBrandsRequest)
         yield put(FormActions.getBrandSuccess(brands))
     } catch(err){
         console.log(err)
-        yield put(FormActions.getBrandFailure([]))
+        yield put(FormActions.getBrandFail([]))
     }
 } 
+
+export function* getCars(){
+    try{
+        const vehicles = yield call(services.searchVehiclesRequest)
+        yield put(ListActions.getCarsSuccess(vehicles))
+    }catch(err){
+        console.log(err)
+        yield put(ListActions.getCarsFail([]))
+    }
+}
