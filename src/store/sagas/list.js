62cba00aa0e9a4b0d7477617859316f56682d8a2
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 15:40:09 -0200

    refactory: remove logs

--- src/store/sagas/list.js
+++ src/store/sagas/list.js
@@ -2,11 +2,9 @@ import { call, put } from 'redux-saga/effects'
 import { services } from '../../services'
 import { Creators as FormActions } from '../actions/form'
 
-export function* getBrands(action){
+export function* getBrands(){
     try{
         const brands = yield call(services.getBrands)
-        console.log('retorno')
-        console.log(brands)
         yield put(FormActions.getBrandSuccess(brands))
     } catch(err){
         console.log(err)
