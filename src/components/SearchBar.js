51ffa4fa4993498f99d3597328b0c7fe1fed1fe3
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 14:16:37 -0200

    feat: connected searchBar component on store

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button'
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Creators as FormCreators } from '../store/actions/form'
 
 const styles = {
     root: {
@@ -14,25 +17,30 @@ const styles = {
     }
   };
 
-const SearchBar = (props) => {
-    const { classes } = props;
-    return(
-        <div className="search-bar">
-            <TextField
-                id="outlined-bare"
-                className={classes.root}
-                placeholder="Pesquise por um veículo"
-                margin="normal"
-                variant="outlined"
-                InputProps={{
-                    className: classes.input
-                }}
-            />
-            <Link to={props.formLink}>
-                <Button variant="contained" className='button'>Cadastrar</Button>
-            </Link>
-        </div>
-    )
+class SearchBar extends Component{
+    
+    render(){
+        const { classes } = this.props;
+        return(
+            <div className="search-bar">
+                <TextField
+                    id="outlined-bare"
+                    className={classes.root}
+                    placeholder="Pesquise por um veículo"
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        className: classes.input
+                    }}
+                />
+                <Link to={this.props.formLink}>
+                    <Button variant="contained" onClick={this.props.startAdd} className='button'>Cadastrar</Button>
+                </Link>
+            </div>
+        )
+    }
 }
 
-export default withStyles(styles)(SearchBar)
+const mapDispatchToProps = dispatch => bindActionCreators(FormCreators, dispatch)
+
+export default connect(null, mapDispatchToProps)(withStyles(styles)(SearchBar))
