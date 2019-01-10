4bb65b48b6e7f1a6b22b447dfe908ed3770a344e
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-10 17:40:59 -0200

    feat: Added action and reducer for search cars from api

--- src/store/actions/list.js
+++ src/store/actions/list.js
@@ -1,7 +1,9 @@
 
 export const Types = {
-    ADD_VEHICLE: 'ADD_VEHICLE',
-    SEARCH_VEHICLES: 'SEARCH_VEHICLES'
+    ADD_VEHICLE: 'list/ADD_VEHICLE',
+    START_SEARCH_VEHICLES: 'list/START_SEARCH_VEHICLES',
+    GET_CARS_SUCCESS: 'list/GET_CARS_SUCCESS',
+    GET_CARS_FAIL: 'listGET_CARS_FAIL'
 }
 
 export const Creators = {
@@ -9,8 +11,15 @@ export const Creators = {
         type: Types.ADD_VEHICLE,
         vehicle
     }),
-    searchVehicles: (list) => ({
-        type: Types.SEARCH_VEHICLES,
-        list 
+    startSearchVehicles: () => ({
+        type: Types.START_SEARCH_VEHICLES
+    }),
+    getCarsSuccess: (vehicles) => ({
+        type: Types.GET_CARS_SUCCESS,
+        vehicles
+    }),
+    getCarsFail: () => ({
+        type: Types.GET_CARS_FAIL,
+        vehicles: []
     })
 }
--- src/store/reducers/list.js
+++ src/store/reducers/list.js
@@ -1,17 +1,31 @@
 import { Types } from '../actions/list'
 
 const INITIAL_STATE = {
-    vehicles: []
+    vehicles: [],
+    isFetching: false
 }
 
 export default function list( state = INITIAL_STATE, action){
-
     switch(action.type){
         case Types.ADD_VEHICLE:
             return {
                     vehicles: [ ...state.vehicles, {...action.vehicle}
                     ]
             }
+
+        case Types.START_SEARCH_VEHICLES:
+            return {
+                vehicles: [...state.vehicles],
+                isFetching: true
+            }
+        
+        case Types.GET_CARS_SUCCESS:
+        case Types.GET_CARS_FAIL:
+            return {
+                ...state,
+                vehicles: action.vehicles,
+                isFetching: false
+            }    
 
         default:
             return {
